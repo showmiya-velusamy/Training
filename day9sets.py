f5ebe2738f7fc194b93d3807716059888445b2dd
showmiya-velusamy <showmiyavelusamy@gmail.com>
2024-05-07 11:23:04 +0530

    sets

--- day9sets.py
+++ day9sets.py
@@ -1,3 +1,23 @@
 colors = ["red", "blue", "red", "blue", "green"]
 print(set(colors))
 
+# Output
+# [6.4, 1.414, 11.66, 13.45]
+# Task 1 : Finding the distance between given coordinates and origin
+coordinates = [(5,4) , (1,1), (6,10), (9,10)]
+distance = []
+for (x,y) in coordinates:
+    distance.append(((x**2) + (y**2))**0.5)
+print(distance)
+
+#task-2- for loop and unpacked
+d=[]
+for coordinate in coordinates:
+    x,y =coordinate
+    d.append((x**2 + y**2)**0.5)
+print(d)
+
+#task 3-list comprehension
+coordinates = [(5, 4), (1, 1), (6, 10), (9, 10)]
+distance=[round((x**2 + y**2)**0.5) for x,y in coordinates]
+print(distance)
